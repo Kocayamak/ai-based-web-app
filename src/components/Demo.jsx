b82a3bbb9dca02cf6f80b149d649d625fa9fc875
Kocayamak <mikailkocayamak@gmail.com>
2023-07-21 22:35:26 +0300

    Demo Components finished

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,10 +1,47 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useEffect, useState } from "react";
+// eslint-disable-next-line no-unused-vars
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
-  return (
-    <div>Demo</div>
-  )
-}
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
 
-export default Demo
+  const handleSubmit = async (e) => {
+    alert("Link kısaltılıyor..." , e);
+  };
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="linkIcon.png"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+
+          <input
+            type="url"
+            placeholder="Lütfen bir link giriniz"
+            value={article.url}
+            required
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            className="url_input peer-focus:border-gray-700 peer-focus:text-gray-700"
+          />
+
+          <button type="submit" className="submit_btn">
+            ↳
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Demo;
